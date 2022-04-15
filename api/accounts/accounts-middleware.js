710f18e32c27c6f62f4109aba590f53740d1d0ab
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2022-04-15 15:29:23 -0400

    wip

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -2,12 +2,20 @@ exports.checkAccountPayload = (req, res, next) => {
   // DO YOUR MAGIC
   // Note: you can either write "manual" validation logic
   // or use the Yup library (not currently installed)
+  console.log('checkAccountPayloadMiddleware')
+  next()
 }
 
 exports.checkAccountNameUnique = (req, res, next) => {
   // DO YOUR MAGIC
+  console.log('checkAccountNameUniqueMiddleware')
+
+  next()
 }
 
 exports.checkAccountId = (req, res, next) => {
   // DO YOUR MAGIC
+  console.log('checkAccountIdMiddleware')
+
+  next()
 }
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const md = require('./accounts-middleware')
 
 router.get('/', (req, res, next) => {
   // DO YOUR MAGIC
@@ -9,7 +10,7 @@ router.get('/', (req, res, next) => {
   }
 })
 
-router.get('/:id', (req, res, next) => {
+router.get('/:id', md.checkAccountId, (req, res, next) => {
   // DO YOUR MAGIC
   try {
     res.json('get account by id')
@@ -18,25 +19,36 @@ router.get('/:id', (req, res, next) => {
   }
 })
 
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
-  try {
-    res.json('post account')
-  } catch (err) {
-    next(err)
+router.post(
+  '/', 
+  md.checkAccountPayload, 
+  md.checkAccountNameUnique,
+  (req, res, next) => {
+    // DO YOUR MAGIC
+    try {
+      res.json('post account')
+    } catch (err) {
+      next(err)
+    }
   }
-})
+)
 
-router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-  try {
-    res.json('update account')
-  } catch (err) {
-    next(err)
+router.put(
+  '/:id', 
+  md.checkAccountId, 
+  md.checkAccountPayload, 
+  md.checkAccountNameUnique, 
+  (req, res, next) => {
+    // DO YOUR MAGIC
+    try {
+      res.json('update account')
+    } catch (err) {
+      next(err)
+    }
   }
-});
+);
 
-router.delete('/:id', (req, res, next) => {
+router.delete('/:id', md.checkAccountId, (req, res, next) => {
   // DO YOUR MAGIC
   try {
     res.json('delete account')
