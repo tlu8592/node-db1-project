3b541e27974d155bf3d29c8014dd6c7591092add
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2022-04-15 15:36:46 -0400

    wip

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,9 +1,15 @@
+const db = require('../../data/db-config')
+
 const getAll = () => {
   // DO YOUR MAGIC
+  // select * from accounts;
+  return db('accounts');
 }
 
 const getById = id => {
   // DO YOUR MAGIC
+  // select * from accounts where id = 1;
+  return db('accounts').where('id', id).first()
 }
 
 const create = account => {
