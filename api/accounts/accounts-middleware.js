9c294b5c9f15cafdfc0be8b37b4c8d1cb81b3f1a
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2022-04-15 15:40:31 -0400

    wip

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,3 +1,5 @@
+const Account = require('./accounts-model')
+
 exports.checkAccountPayload = (req, res, next) => {
   // DO YOUR MAGIC
   // Note: you can either write "manual" validation logic
@@ -13,9 +15,17 @@ exports.checkAccountNameUnique = (req, res, next) => {
   next()
 }
 
-exports.checkAccountId = (req, res, next) => {
+exports.checkAccountId = async (req, res, next) => {
   // DO YOUR MAGIC
-  console.log('checkAccountIdMiddleware')
-
-  next()
+  try {
+    const account = await Account.getById(req.params.id)
+    if (!account) {
+      next({ status: 404, message: 'account not found' })
+    } else {
+      req.account = account
+      next()
+    }
+  } catch (err) {
+    next(err)
+  }
 }
